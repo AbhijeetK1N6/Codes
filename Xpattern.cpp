60b27f7e953653092198bf455c49b8d17b073e0b
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2022-12-17 17:40:53 +0530

    Create Xpattern.cpp

---
+++ Xpattern.cpp
@@ -0,0 +1,37 @@
+/*
+Note: This section is language agnostic. You can choose to code in ANY anguage. There is no necessary advantage to choosing to code in multiple Languages.
+
+Hari is a civil engineer who is designing a fountain in square shape with water sprinklers in the edges with n number of steps. He needs to draw a sketch of the fountain in top view with the step number at the edges of the square.
+
+Write a program to help him in printing the pattern with n number of steps.
+
+Input format:
+Input is an positive integer describing the step levels of the fountain
+
+Output format:
+Output consists of the pattern of numbers as described in the question for n number of step levels.
+
+Sample testcases:
+Input 1 : 5
+Output 1 : 
+1        1
+ 2     2
+  3   3
+   4 4
+    5
+   4 4 
+  3   3
+ 2     2
+1       1 
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //To get this code in C++ message me on Telegram @avi_akshit
+}
+
+
+
